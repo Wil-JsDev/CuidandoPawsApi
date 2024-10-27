52e01b58637b3a9ccf21c983eac46d76ede13640
Wilmer De La Cruz <wilmerdelacruz.dev@gmail.com>
2024-10-27 16:25:27 -0400

    add models

---
+++ CuidandoPawsApi.Domain/Models/Adoption.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CuidandoPawsApi.Domain.Models
+{
+    public class Adoption
+    {
+        public  int Id { get; set; }
+
+        public DateTime AdoptionDate { get; set; }
+
+        public bool status { get; set; }
+
+        public string? Notes { get; set; }
+    }
+}
---
+++ CuidandoPawsApi.Domain/Models/Pets.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CuidandoPawsApi.Domain.Models
+{
+    public class Pets
+    {
+        public int Id { get; set; }
+
+        public string? NamePaws { get; set; }
+
+        public string? Bred { get; set; }
+
+        public int Age { get; set; }
+
+        public string? Color { get; set; }
+
+        public bool AdoptionStatus { get; set; }
+
+        public string? Notes { get; set; }
+
+        public DateTime DateOfEntry { get; set; } = DateTime.UtcNow;
+
+        public bool Gender { get; set; }
+
+        public DateTime? CreatedAt { get; set; } = DateTime.UtcNow;
+
+        public int IdSpecie { get; set; }
+
+        public ICollection<Species>? Specie { get; set; }
+
+    }
+}
