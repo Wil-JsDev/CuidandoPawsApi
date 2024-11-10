d7f8906c57f136a0fa70cb326fa1a0109fc34fff
Wilmer De La Cruz <wilmerdelacruz.dev@gmail.com>
2024-11-10 14:45:29 -0400

    feat(migrations): add migrations and update species model

---
+++ CuidandoPawsApi.Infrastructure.Persistence/Migrations/20241110150803_AddProp-ServiceCatalogProperty.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CuidandoPawsApi.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPropServiceCatalogProperty : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsAvaible",
+                table: "ServiceCatalog",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsAvaible",
+                table: "ServiceCatalog");
+        }
+    }
+}
