8fcf146cf205a5469c6b11eb5d7763e9af5d10dc
Wilmer De La Cruz <wilmerdelacruz.dev@gmail.com>
2024-11-15 10:44:08 -0400

    feat: add use cases service catalog

---
+++ CuidandoPawsApi.Domain/Ports/UseCase/ServiceCatalog/IGetByIdServiceCatalog.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CuidandoPawsApi.Domain.Ports.UseCase.ServiceCatalog
+{
+    public interface IGetByIdServiceCatalog<TDto>
+    {
+        Task<TDto> GetByIdAsync(int id, CancellationToken cancellationToken);
+    }
+}
