a2f263c125db860830f561831310144f92f8f3fb
Wilmer De La Cruz <wilmerdelacruz.dev@gmail.com>
2024-11-15 10:42:57 -0400

    feat: add use cases medical record

---
+++ CuidandoPawsApi.Domain/Ports/UseCase/MedicalRecord/IUpdateMedicalRecord.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CuidandoPawsApi.Domain.Ports.UseCase.MedicalRecord
+{
+    public interface IUpdateMedicalRecord<TDtoStatus,TDTo>
+    {
+        Task<TDTo> UpdateAsync(int id,TDtoStatus dtoStatus);
+    }
+}
