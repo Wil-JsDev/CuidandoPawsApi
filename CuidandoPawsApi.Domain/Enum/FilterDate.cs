bc0d66ba5dfba537f61516aa4409998daf923c9b
Wilmer De La Cruz <wilmerdelacruz.dev@gmail.com>
2024-11-15 10:37:35 -0400

    feat: add enum

---
+++ CuidandoPawsApi.Domain/Enum/FilterDate.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CuidandoPawsApi.Domain.Enum
+{
+    public enum FilterDate
+    {
+        LastDay,
+        LastThreeDay,
+        LasWeek
+    }
+    
+}
