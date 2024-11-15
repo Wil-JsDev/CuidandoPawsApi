2fd7260abd62e5c0cff93a6d270380991055b077
Wilmer De La Cruz <wilmerdelacruz.dev@gmail.com>
2024-11-15 10:44:41 -0400

    feat: add use cases species

---
+++ CuidandoPawsApi.Domain/Ports/UseCase/Species/IGetByIdSpecies.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CuidandoPawsApi.Domain.Ports.UseCase.Species
+{
+    public interface IGetByIdSpecies <TDto>
+    {
+        Task<TDto> GetById (int id, CancellationToken cancellationToken);
+    }
+}
---
+++ CuidandoPawsApi.Domain/Ports/UseCase/Species/IUpdateSpecies.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CuidandoPawsApi.Domain.Ports.UseCase.Species
+{
+    public interface IUpdateSpecies<TDtoStatus,TDto>
+    {
+        Task<TDto> UpdateAsync(int Id, TDtoStatus dtoStatus);
+    }
+}
