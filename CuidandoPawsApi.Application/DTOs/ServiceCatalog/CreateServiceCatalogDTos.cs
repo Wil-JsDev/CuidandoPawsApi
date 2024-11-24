45612e3e12e545a929db6de3c3efa004214d7f9f
Wilmer De La Cruz <wilmerdelacruz.dev@gmail.com>
2024-11-24 10:51:54 -0400

    feat: add service catalog dtos

---
+++ CuidandoPawsApi.Application/DTOs/ServiceCatalog/CreateServiceCatalogDTos.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CuidandoPawsApi.Application.DTOs.ServiceCatalog
+{
+    public class CreateServiceCatalogDTos
+    {
+        public string? NameService { get; set; }
+
+        public string? DescriptionService { get; set; }
+
+        public decimal Price { get; set; }
+
+        public string? Type { get; set; }
+
+        public int Duration { get; set; }
+    }
+}
