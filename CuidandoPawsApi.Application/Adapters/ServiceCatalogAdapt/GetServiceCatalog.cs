d4e88574ae8b466cc7a894614c0f305685f80f49
Wilmer De La Cruz <wilmerdelacruz.dev@gmail.com>
2024-11-24 10:59:17 -0400

    feat: add use case service catalog

---
+++ CuidandoPawsApi.Application/Adapters/ServiceCatalogAdapt/GetServiceCatalog.cs
@@ -0,0 +1,30 @@
+﻿using AutoMapper;
+using CuidandoPawsApi.Application.DTOs.ServiceCatalog;
+using CuidandoPawsApi.Domain.Ports.Repository;
+using CuidandoPawsApi.Domain.Ports.UseCase.ServiceCatalog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CuidandoPawsApi.Application.Adapters.ServiceCatalogAdapt
+{
+    public class GetServiceCatalog : IGetServiceCatalog<ServiceCatalogDTos>
+    {
+        private readonly IServiceCatalogRepository _serviceCatalogRepository;
+        private readonly IMapper _mapper;
+
+        public GetServiceCatalog(IServiceCatalogRepository serviceCatalogRepository, IMapper mapper)
+        {
+            _serviceCatalogRepository = serviceCatalogRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<IEnumerable<ServiceCatalogDTos>> GetAllAsync(CancellationToken cancellationToken)
+        {
+            var serviceCatalogs = await _serviceCatalogRepository.GetAllAsync(cancellationToken);
+            return serviceCatalogs.Select(x => _mapper.Map<ServiceCatalogDTos>(x));
+        }
+    }
+}
