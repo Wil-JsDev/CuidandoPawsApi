e48cf7bf9eea50c1e783dc67968eb214110fbe12
Wilmer De La Cruz <wilmerdelacruz.dev@gmail.com>
2024-12-14 19:36:00 -0400

    feat: add migration for update database

---
+++ CuidandoPawsApi.Infrastructure.Persistence/Migrations/20241214222100_AddRelationAppoinment.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CuidandoPawsApi.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddRelationAppoinment : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ServiceCatalog",
+                table: "Appoinment");
+
+            migrationBuilder.AddForeignKey(
+                name: "Fk_Appoinment",
+                table: "Appoinment",
+                column: "IdServiceCatalog",
+                principalTable: "ServiceCatalog",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "Fk_Appoinment",
+                table: "Appoinment");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ServiceCatalog",
+                table: "Appoinment",
+                column: "IdServiceCatalog",
+                principalTable: "ServiceCatalog",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
