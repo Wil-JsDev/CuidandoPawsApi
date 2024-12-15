5a5c58f636c19afaabaffb6efd307ce4383a93d7
Wilmer De La Cruz <wilmerdelacruz.dev@gmail.com>
2024-12-15 15:15:36 -0400

    feat: add migrations for key initial value

---
+++ CuidandoPawsApi.Infrastructure.Persistence/Migrations/20241215181622_NewConfigurationPetsAndSpecies.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CuidandoPawsApi.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class NewConfigurationPetsAndSpecies : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("ALTER SEQUENCE \"Pets_Id_seq\" RESTART WITH 10000;");
+            migrationBuilder.Sql("ALTER SEQUENCE \"Species_Id_seq\" RESTART WITH 10000;");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("ALTER SEQUENCE \"Pets_Id_seq\" RESTART WITH 10000;");
+            migrationBuilder.Sql("ALTER SEQUENCE \"Species_Id_seq\" RESTART WITH 10000;");
+        }
+    }
+}
