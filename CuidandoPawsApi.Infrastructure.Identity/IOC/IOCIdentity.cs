fabe907d3265bf7c3bf80d0d3b3d56aa56cec33a
Wilmer De La Cruz <wilmerdelacruz.dev@gmail.com>
2024-12-17 11:10:30 -0400

    build: add extension method for configuration

--- CuidandoPawsApi.Infrastructure.Identity/IOC/IOCIdentity.cs
+++ CuidandoPawsApi.Infrastructure.Identity/IOC/IOCIdentity.cs
@@ -1,7 +1,32 @@
-﻿namespace CuidandoPawsApi.Infrastructure.Identity.IOC
+﻿using CuidandoPawsApi.Infrastructure.Identity.Context;
+using CuidandoPawsApi.Infrastructure.Identity.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CuidandoPawsApi.Infrastructure.Identity.IOC
 {
-    public class IOCIdentity
+    public static class IOCIdentity
     {
 
+        public static void AddIdentity(this IServiceCollection services, IConfiguration configuration)
+        {
+
+            #region IdentityContext
+            services.AddDbContext<IdentityContext>(options =>
+            {
+                options.UseNpgsql(configuration.GetConnectionString("CuidandoPaswsDbIndetity"), 
+                    b => b.MigrationsAssembly("CuidandoPawsApi.Infrastructure.Identity"));
+            });
+
+            #endregion
+
+            #region Identity
+            services.AddIdentity<User,IdentityRole>().AddEntityFrameworkStores<IdentityContext>()
+                                                       .AddDefaultTokenProviders();
+
+            #endregion
+        }
     }
 }
