e11f4562ea5bd020e8098fe1b731850756baef04
Wilmer De La Cruz <wilmerdelacruz.dev@gmail.com>
2024-12-18 11:35:09 -0400

    feat: add account interface use case

---
+++ CuidandoPawsApi.Domain/Ports/UseCase/Account/ICreateAccount.cs
@@ -0,0 +1,20 @@
+﻿using CuidandoPawsApi.Domain.Enum;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CuidandoPawsApi.Domain.Ports.UseCase.Account
+{
+    public interface ICreateAccount <TResponse,TRequest>
+        where TResponse : class 
+        where TRequest : class
+    {
+        Task<TResponse> RegisterPetOwnerAsync(TRequest request, string origin);
+
+        Task<TResponse> RegisterAdminAsync(TRequest request, string origin);
+
+        Task<TResponse> RegisterCaregiverAsync(TRequest resquest, string origin);
+    }
+}
---
+++ CuidandoPawsApi.Domain/Ports/UseCase/Account/IUpdateAccountDetails.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CuidandoPawsApi.Domain.Ports.UseCase.Account
+{
+    public interface IUpdateAccountDetails<TDto, TStatusDto> 
+        where TStatusDto : class
+        where TDto : class
+    {
+        Task<TDto> UpdateAccountDetailsAsync(TStatusDto status, string id);
+    }
+}
