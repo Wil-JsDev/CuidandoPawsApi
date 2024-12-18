33031577010ef8ef49fd90ac03313d03220a98d6
Wilmer De La Cruz <wilmerdelacruz.dev@gmail.com>
2024-12-18 11:32:38 -0400

    feat: add account dtos

---
+++ CuidandoPawsApi.Application/DTOs/Account/Register/RegisterResponse.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CuidandoPawsApi.Application.DTOs.Account.Register
+{
+    public class RegisterResponse
+    {
+        public int StatusCode { get; set; }
+
+        public string? Error { get; set; }
+
+        public bool? HasError { get; set; }
+    }
+}
