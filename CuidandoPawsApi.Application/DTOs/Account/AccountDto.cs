f3b0b068d7e083ebfc136a90a0c5c8a268d42a62
Wilmer De La Cruz <wilmerdelacruz.dev@gmail.com>
2024-12-19 19:14:45 -0400

    feat: add account and update account dtos

---
+++ CuidandoPawsApi.Application/DTOs/Account/AccountDto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CuidandoPawsApi.Application.DTOs.Account
+{
+    public class AccountDto
+    {
+        public string? FirstName { get; set; }
+
+        public string? LastName { get; set; }
+
+        public string? Username { get; set; }
+
+        public string? PhoneNumber { get; set; }
+
+        public string? Email { get; set; }
+    }
+}
