8ea5010f4db647ae11e1d6638f3c50236a429a59
Wilmer De La Cruz <wilmerdelacruz.dev@gmail.com>
2024-12-19 16:51:02 -0400

    build: add account use case di

--- CuidandoPawsApi.Infrastructure.Identity/IOC/IOCIdentity.cs
+++ CuidandoPawsApi.Infrastructure.Identity/IOC/IOCIdentity.cs
@@ -1,4 +1,11 @@
-﻿using CuidandoPawsApi.Infrastructure.Identity.Context;
+﻿using CuidandoPawsApi.Application.DTOs.Account;
+using CuidandoPawsApi.Application.DTOs.Account.Authenticate;
+using CuidandoPawsApi.Application.DTOs.Account.Password.Forgot;
+using CuidandoPawsApi.Application.DTOs.Account.Password.Reset;
+using CuidandoPawsApi.Application.DTOs.Account.Register;
+using CuidandoPawsApi.Domain.Ports.UseCase.Account;
+using CuidandoPawsApi.Infrastructure.Identity.Adapters;
+using CuidandoPawsApi.Infrastructure.Identity.Context;
 using CuidandoPawsApi.Infrastructure.Identity.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
@@ -26,6 +33,18 @@ namespace CuidandoPawsApi.Infrastructure.Identity.IOC
             services.AddIdentity<User,IdentityRole>().AddEntityFrameworkStores<IdentityContext>()
                                                        .AddDefaultTokenProviders();
 
+            #endregion
+
+            #region AccountUseCase
+            services.AddScoped<ICreateAccount<RegisterResponse, RegisterRequest>, CreateAccount>();
+            services.AddScoped<IConfirmAccount, ConfirmAccount>();
+            services.AddScoped<IAuthenticateAccount<AuthenticateResponse, AuthenticateRequest>, AuthenticateAccount>();
+            services.AddScoped<ILogout,Logout>();
+            services.AddScoped<IForgotPassword<ForgotResponse, ForgotRequest>,ForgotPassword>();
+            services.AddScoped<IResetPassword<ResetPasswordResponse, ResetPasswordRequest>, ResetPassword>();
+            services.AddScoped<IGetAccountDetails<AccountDto>, GetAccountDetails>();
+            services.AddScoped<IUpdateAccountDetails<AccountDto,UpdateAccountDTo>, UpdateAccountDetails>();
+            services.AddScoped<IDeleteAccount, DeleteAccount>();
             #endregion
         }
     }
