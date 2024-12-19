70b688c7b1e78f7a0682d865b969525f3dd01ad4
Wilmer De La Cruz <wilmerdelacruz.dev@gmail.com>
2024-12-19 19:05:57 -0400

    build: add configuration for jwt

--- CuidandoPawsApi.Infrastructure.Identity/IOC/IOCIdentity.cs
+++ CuidandoPawsApi.Infrastructure.Identity/IOC/IOCIdentity.cs
@@ -1,16 +1,23 @@
 ﻿using CuidandoPawsApi.Application.DTOs.Account;
 using CuidandoPawsApi.Application.DTOs.Account.Authenticate;
+using CuidandoPawsApi.Application.DTOs.Account.JWT;
 using CuidandoPawsApi.Application.DTOs.Account.Password.Forgot;
 using CuidandoPawsApi.Application.DTOs.Account.Password.Reset;
 using CuidandoPawsApi.Application.DTOs.Account.Register;
 using CuidandoPawsApi.Domain.Ports.UseCase.Account;
+using CuidandoPawsApi.Domain.Settings;
 using CuidandoPawsApi.Infrastructure.Identity.Adapters;
 using CuidandoPawsApi.Infrastructure.Identity.Context;
 using CuidandoPawsApi.Infrastructure.Identity.Models;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using Newtonsoft.Json;
+using System.Text;
 
 namespace CuidandoPawsApi.Infrastructure.Identity.IOC
 {
@@ -32,6 +39,62 @@ namespace CuidandoPawsApi.Infrastructure.Identity.IOC
             #region Identity
             services.AddIdentity<User,IdentityRole>().AddEntityFrameworkStores<IdentityContext>()
                                                        .AddDefaultTokenProviders();
+
+            services.Configure<JWTSettings>(configuration.GetSection("JWTSettings"));
+
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(options =>
+            {
+                options.RequireHttpsMetadata = false;
+                options.SaveToken = false;
+
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = true,
+                    ClockSkew = TimeSpan.Zero,
+                    ValidIssuer = configuration["JWTSettings:Issuer"],
+                    ValidAudience = configuration["JWTSettings:Audience"],
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["JWTSettings:Key"]))
+                };
+
+                options.Events = new JwtBearerEvents
+                {
+                    OnAuthenticationFailed = c =>
+                    {
+                        //Cuando el codigo explote, porque algo no se manejo
+                        c.NoResult();
+                        c.Response.StatusCode = 500;
+                        c.Response.ContentType = "text/plain";
+                        return c.Response.WriteAsync(c.Exception.ToString());
+                    },
+                    OnChallenge = c =>
+                    {
+                        //No estas autorizado o fue un token invalido
+                        c.HandleResponse();
+                        c.Response.StatusCode = 401;
+                        c.Response.ContentType = "application/json";
+                        var result = JsonConvert.SerializeObject(new JwtResponse { HasError = true, Error = "You are not authorized"});
+                        return c.Response.WriteAsync(result);
+                    },
+                    OnForbidden = c =>
+                    {
+                        //Es para cuando el usuario no tiene permiso para entrar
+                        c.Response.StatusCode = 403;
+                        c.Response.ContentType  = "application/json";
+                        var result = JsonConvert.SerializeObject(new JwtResponse { HasError = true, Error = "You are not authorized to access this resource"});
+                        return c.Response.WriteAsync(result);
+                    }
+
+
+                };
+            });
+
 
             #endregion
 
