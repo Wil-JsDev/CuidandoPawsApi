c21903a03676158b5628b6690a3a855d216b6836
Wilmer De La Cruz <wilmerdelacruz.dev@gmail.com>
2024-12-22 10:43:02 -0400

    feat: add validator extension method

---
+++ CuidandoPawsApi.Infrastructure.Api/Extensions/AddValidator.cs
@@ -0,0 +1,15 @@
+﻿using CuidandoPawsApi.Application.DTOs.Appoinment;
+using CuidandoPawsApi.Infrastructure.Api.Validations.Appoinment;
+using FluentValidation;
+
+namespace CuidandoPawsApi.Infrastructure.Api.Extensions
+{
+    public static class AddValidator
+    {
+        public static void AddValidations(this IServiceCollection services)
+        {
+            services.AddScoped<IValidator<CreateUpdateAppoinmentDTos>, CreateUpdateAppoinment>();
+            
+        }
+    }
+}
