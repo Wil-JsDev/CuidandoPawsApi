07028109931f0eb809720193acb0201333c35cd1
Wilmer De La Cruz <wilmerdelacruz.dev@gmail.com>
2024-12-22 17:24:48 -0400

    feat: add result pattern use case get by id appoinment

--- CuidandoPawsApi.Domain/Ports/UseCase/Appoinment/IGetByIdAppoinment.cs
+++ CuidandoPawsApi.Domain/Ports/UseCase/Appoinment/IGetByIdAppoinment.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CuidandoPawsApi.Domain.Utils;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,6 @@ namespace CuidandoPawsApi.Domain.Ports.UseCase.Appoinment
 {
     public interface IGetByIdAppoinment<TDto>
     {
-        Task<TDto> GetByIdAsync(int id, CancellationToken cancellationToken);
+        Task <ResultT<TDto>> GetByIdAsync(int id, CancellationToken cancellationToken);
     }
 }
