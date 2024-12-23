56497b8362657ddc47986808cd67b55137898198
Wilmer De La Cruz <wilmerdelacruz.dev@gmail.com>
2024-12-23 19:16:20 -0400

    chore: add string sort parameter

--- CuidandoPawsApi.Application/Adapters/SpeciesAdapt/GetSpeciesOrderById.cs
+++ CuidandoPawsApi.Application/Adapters/SpeciesAdapt/GetSpeciesOrderById.cs
@@ -23,22 +23,33 @@ namespace CuidandoPawsApi.Application.Adapters.SpeciesAdapt
             _mapper = mapper;
         }
 
-        public async Task <ResultT<IEnumerable<SpeciesDTos>>> GetOrderedByIdAsync(string direction,CancellationToken cancellationToken)
+        public async Task <ResultT<IEnumerable<SpeciesDTos>>> GetOrderedByIdAsync(string sort,string direction,CancellationToken cancellationToken)
         {
 
-            if (direction == "asc".ToLower())
+            if (sort == "id".ToLower() && direction == "asc".ToLower())
             {
                 var speciesOrderById = await _speciesRepository.GetOrdereByIdAscSpeciesAsync(cancellationToken);
                 var speciesDto = _mapper.Map<IEnumerable<SpeciesDTos>>(speciesOrderById);
                 return ResultT<IEnumerable<SpeciesDTos>>.Success(speciesDto);
             }
-            else if (direction == "desc".ToLower())
+            else if (sort == "id".ToLower() && direction == "desc".ToLower())
             {
                 var speciesOrderByIdDesc = await _speciesRepository.GetOrdereByIdDescSpeciesAsync(cancellationToken);
                 var speciesDto = _mapper.Map<IEnumerable<SpeciesDTos>>(speciesOrderByIdDesc);
                 return ResultT<IEnumerable<SpeciesDTos>>.Success(speciesDto);
             }
-
+            else if (sort == "name".ToLower() && direction == "asc".ToLower())
+            {
+                var speciesOrderByNameAsc = await _speciesRepository.GetOrdereByNameAscSpeciesAsync(cancellationToken);
+                var speciesDto = _mapper.Map<IEnumerable<SpeciesDTos>>(speciesOrderByNameAsc);
+                return ResultT<IEnumerable<SpeciesDTos>>.Success(speciesDto);
+            }
+            else if (sort == "name".ToLower() && direction == "desc".ToLower())
+            {
+                var speciesOrderByNameDesc = await _speciesRepository.GetOrdereByNameDescSpeciesAsync(cancellationToken);
+                var speciesDto = _mapper.Map<IEnumerable<SpeciesDTos>>(speciesOrderByNameDesc);
+                return ResultT<IEnumerable<SpeciesDTos>>.Success(speciesDto);
+            }
             return ResultT<IEnumerable<SpeciesDTos>>.Failure(Error.Failure("400","Invalid direction parameter. Accepted values are 'asc' or 'desc'."));
         }
     }
