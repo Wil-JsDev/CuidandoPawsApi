74707105b0699f53d6afd390263689deb4655ca2
Wilmer De La Cruz <wilmerdelacruz.dev@gmail.com>
2024-12-24 11:23:42 -0400

    feat: add pattern result to update medical record

--- CuidandoPawsApi.Application/Adapters/MedicalRecordAdapt/UpdateMedicalRecord.cs
+++ CuidandoPawsApi.Application/Adapters/MedicalRecordAdapt/UpdateMedicalRecord.cs
@@ -4,6 +4,7 @@ using CuidandoPawsApi.Application.DTOs.MedicalRecord;
 using CuidandoPawsApi.Domain.Models;
 using CuidandoPawsApi.Domain.Ports.Repository;
 using CuidandoPawsApi.Domain.Ports.UseCase.MedicalRecord;
+using CuidandoPawsApi.Domain.Utils;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -24,7 +25,7 @@ namespace CuidandoPawsApi.Application.Adapters.MedicalRecordAdapt
             _mapper = mapper;
         }
 
-        public async Task<MedicalRecordDTos> UpdateAsync(int id, CreateUpdateMedicalRecordDTos dtoStatus, CancellationToken cancellationToken)
+        public async Task <ResultT<MedicalRecordDTos>> UpdateAsync(int id, CreateUpdateMedicalRecordDTos dtoStatus, CancellationToken cancellationToken)
         {
             var medicalRecord = await _medicalRecordRepository.GetByIdAsync(id, cancellationToken);
             if (medicalRecord != null)
@@ -32,10 +33,10 @@ namespace CuidandoPawsApi.Application.Adapters.MedicalRecordAdapt
                 medicalRecord = _mapper.Map<CreateUpdateMedicalRecordDTos, MedicalRecord>(dtoStatus, medicalRecord);
                 await _medicalRecordRepository.UpdateAsync(medicalRecord);
                 var medicalRecordDTo = _mapper.Map<MedicalRecordDTos>(medicalRecord);
-                return medicalRecordDTo;
+                return ResultT<MedicalRecordDTos>.Success(medicalRecordDTo);
             }
 
-            return null;
+            return ResultT<MedicalRecordDTos>.Failure(Error.NotFound("404", "id not found"));
         }
     }
 }
--- CuidandoPawsApi.Domain/Ports/UseCase/MedicalRecord/IUpdateMedicalRecord.cs
+++ CuidandoPawsApi.Domain/Ports/UseCase/MedicalRecord/IUpdateMedicalRecord.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CuidandoPawsApi.Domain.Utils;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,6 @@ namespace CuidandoPawsApi.Domain.Ports.UseCase.MedicalRecord
 {
     public interface IUpdateMedicalRecord<TDtoStatus,TDTo>
     {
-        Task<TDTo> UpdateAsync(int id,TDtoStatus dtoStatus, CancellationToken cancellationToken);
+        Task <ResultT<TDTo>> UpdateAsync(int id,TDtoStatus dtoStatus, CancellationToken cancellationToken);
     }
 }
