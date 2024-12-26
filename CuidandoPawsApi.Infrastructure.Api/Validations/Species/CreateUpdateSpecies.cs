46eb441857826d5853c37ad70b46dd45057306f4
Wilmer De La Cruz <wilmerdelacruz.dev@gmail.com>
2024-12-26 12:08:49 -0400

    chore: space in the message

--- CuidandoPawsApi.Infrastructure.Api/Validations/Species/CreateUpdateSpecies.cs
+++ CuidandoPawsApi.Infrastructure.Api/Validations/Species/CreateUpdateSpecies.cs
@@ -8,7 +8,7 @@ namespace CuidandoPawsApi.Infrastructure.Api.Validations.Species
         public CreateUpdateSpecies()
         {
             RuleFor(x => x.DescriptionOfSpecies).NotEmpty().WithMessage("{PropertyName} not null");
-            RuleFor(x => x.DescriptionOfSpecies).MaximumLength(150).WithMessage("{PropertyName}cannot be more than 150 characters ");
+            RuleFor(x => x.DescriptionOfSpecies).MaximumLength(150).WithMessage("{PropertyName} cannot be more than 150 characters ");
         }
     }
 }
