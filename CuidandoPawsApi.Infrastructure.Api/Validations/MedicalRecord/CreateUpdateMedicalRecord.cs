d332c0cb6454e74095f4d6bb29abdb2cf4105179
Wilmer De La Cruz <wilmerdelacruz.dev@gmail.com>
2024-12-26 11:58:50 -0400

    feat: implement fluent validation rules for medical record

---
+++ CuidandoPawsApi.Infrastructure.Api/Validations/MedicalRecord/CreateUpdateMedicalRecord.cs
@@ -0,0 +1,15 @@
+﻿using CuidandoPawsApi.Application.DTOs.MedicalRecord;
+using FluentValidation;
+
+namespace CuidandoPawsApi.Infrastructure.Api.Validations.MedicalRecord
+{
+    public class CreateUpdateMedicalRecord : AbstractValidator<CreateUpdateMedicalRecordDTos>
+    {
+        public CreateUpdateMedicalRecord()
+        {
+            RuleFor(x => x.Treatment).MaximumLength(100).WithMessage("{PrpertyName} cannot be more than 100 characters");
+            RuleFor(x => x.Recommendations).MaximumLength(150).WithMessage("{PropertyName} cannot be more than 150 characters");
+            RuleFor(x => x.IdPet).NotEmpty().WithMessage("{PropertyName} not null");
+        }
+    }
+}
