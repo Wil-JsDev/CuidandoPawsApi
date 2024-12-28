cc63370e5b7b484b1c8eb1561e7acab0e77f38e6
Wilmer De La Cruz <wilmerdelacruz.dev@gmail.com>
2024-12-28 18:38:28 -0400

    chopre: add property name in medical record validation

--- CuidandoPawsApi.Infrastructure.Api/Validations/MedicalRecord/CreateUpdateMedicalRecord.cs
+++ CuidandoPawsApi.Infrastructure.Api/Validations/MedicalRecord/CreateUpdateMedicalRecord.cs
@@ -7,7 +7,7 @@ namespace CuidandoPawsApi.Infrastructure.Api.Validations.MedicalRecord
     {
         public CreateUpdateMedicalRecord()
         {
-            RuleFor(x => x.Treatment).MaximumLength(100).WithMessage("{PrpertyName} cannot be more than 100 characters");
+            RuleFor(x => x.Treatment).MaximumLength(100).WithMessage("{PropertyName} cannot be more than 100 characters");
             RuleFor(x => x.Recommendations).MaximumLength(150).WithMessage("{PropertyName} cannot be more than 150 characters");
             RuleFor(x => x.IdPet).NotEmpty().WithMessage("{PropertyName} not null");
         }
