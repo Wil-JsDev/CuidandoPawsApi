ac909be5e57de7eabb7f7f611641ce3ffd5f1697
Wilmer De La Cruz <wilmerdelacruz.dev@gmail.com>
2025-01-04 17:08:08 -0400

    feat: create extension method for exception handler

---
+++ CuidandoPawsApi.Infrastructure.Api/Extensions/AddExceptionHandler.cs
@@ -0,0 +1,14 @@
+﻿using CuidandoPawsApi.Infrastructure.Api.ExceptionHandling;
+
+namespace CuidandoPawsApi.Infrastructure.Api.Extensions
+{
+    public static class AddExceptionHandler
+    {
+        public static void AddException(this IServiceCollection services)
+        {
+            services.AddExceptionHandler<DbUpdateExceptionHandler>();
+            services.AddExceptionHandler<ValidationExceptionHandler>();
+            services.AddExceptionHandler<GlobalExceptionHandler>();
+        }
+    }
+}
