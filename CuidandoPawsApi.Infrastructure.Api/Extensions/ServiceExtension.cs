83b5fbf0088cef71123fed16d2a3ec20805ed508
Wilmer De La Cruz <wilmerdelacruz.dev@gmail.com>
2025-01-08 20:46:26 -0400

    config: add configuration for jwt

--- CuidandoPawsApi.Infrastructure.Api/Extensions/ServiceExtension.cs
+++ CuidandoPawsApi.Infrastructure.Api/Extensions/ServiceExtension.cs
@@ -14,12 +14,40 @@ namespace CuidandoPawsApi.Infrastructure.Api.Extensions
                 {
                     Version = "v1",
                     Title = "Cuidando Paws Api",
-                    Description = "this API is store",
+                    Description = "API for managing pet care services, including registration, appointments, and medical records",
                     Contact = new OpenApiContact
                     {
                         Name = "Wilmer Jose De La Cruz",
-                        Email = "wilmerjosedelacruz65@gmail.com"
+                        Email = "wilmerdelacruz.dev@gmail.com"
                     }
+                });
+
+                option.DescribeAllParametersInCamelCase();
+                option.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Name = "Authorization",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.ApiKey,
+                    Scheme = "Bearer",
+                    BearerFormat = "JWT",
+                    Description = "Input your Bearer token in this format - Bearer {your token here}"
+                });
+
+                option.AddSecurityRequirement(new OpenApiSecurityRequirement
+                 {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            },
+                            Scheme = "Bearer",
+                            Name = "Bearer",
+                            In = ParameterLocation.Header
+                        }, new List<string>()
+                    },
                 });
             });
 
